c841951ca6cf60512dc47d1aef671aca38286ff6
JacobBarnard <63470336+JacobBarnard@users.noreply.github.com>
2020-05-09 19:06:46 +0000

    Adding buttons and Upload images

---
+++ MyEigthProject/App.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+
+export default function App() {
+  const [selectedImage, setSelecedImage ] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if(permissionResult.granted === false){
+      alert("Permission is required!");
+      return;
+    }
+    
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true){
+      return;
+    }
+
+    setSelecedImage({ localUri: pickerResult.uri});
+  };
+
+  let openSharingDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())){
+      alert('Sharing is not available on this device');
+      return;
+    }
+    Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if(selectedImage !== null){
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: selectedImage.localUri}} style={styles.selImage}/>
+        <TouchableOpacity onPress={openSharingDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>
+            Share my photo
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  
+
+  return (
+    <View style={styles.container}>
+      <Image source = {{uri: "https://brand.colostate.edu/wp-content/uploads/sites/47/2019/01/Ram-Logo-Pantone-357-617.jpg"}}
+        style={styles.logo}>
+      </Image>
+      <Text style={styles.insts}>
+        Press the button below to select an image on your phone!
+      </Text>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress= {openImagePickerAsync}>
+        <Text style={styles.buttonText}>
+          Pick image
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: "#778899",
+  },
+
+  button: {
+    
+    backgroundColor: "#008b8b",
+    padding: 10,
+    borderRadius: 5
+  },
+
+  insts: {
+    fontSize: 18,
+    color: "#9b08f1",
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+
+  logo: {
+    width: 305,
+    height: 300,
+    marginBottom: 20,
+  },
+
+  buttonText:{
+    fontSize: 20,
+    color: "blue"
+  },
+
+  selImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+  }
+
+});
